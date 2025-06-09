c79444507befd90a70d086b3bff48317dbb3e2ad
shouwang <77080979q@gmail.com>
2025-06-09 22:10:44 +0800

    文章评论接口

--- yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/articlecomment/ArticleCommentController.java
+++ yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/controller/admin/articlecomment/ArticleCommentController.java
@@ -1,33 +1,32 @@
 package cn.iocoder.yudao.module.system.controller.admin.articlecomment;
 
-import org.springframework.web.bind.annotation.*;
-import jakarta.annotation.Resource;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.security.access.prepost.PreAuthorize;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Operation;
-
-import jakarta.validation.constraints.*;
-import jakarta.validation.*;
-import jakarta.servlet.http.*;
-import java.util.*;
-import java.io.IOException;
-
+import cn.iocoder.yudao.framework.apilog.core.annotation.ApiAccessLog;
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
-import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.common.util.object.BeanUtils;
-import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
-
 import cn.iocoder.yudao.framework.excel.core.util.ExcelUtils;
-
-import cn.iocoder.yudao.framework.apilog.core.annotation.ApiAccessLog;
-import static cn.iocoder.yudao.framework.apilog.core.enums.OperateTypeEnum.*;
-
-import cn.iocoder.yudao.module.system.controller.admin.articlecomment.vo.*;
+import cn.iocoder.yudao.framework.tenant.core.aop.TenantIgnore;
+import cn.iocoder.yudao.module.system.controller.admin.articlecomment.vo.ArticleCommentPageReqVO;
+import cn.iocoder.yudao.module.system.controller.admin.articlecomment.vo.ArticleCommentRespVO;
+import cn.iocoder.yudao.module.system.controller.admin.articlecomment.vo.ArticleCommentSaveReqVO;
 import cn.iocoder.yudao.module.system.dal.dataobject.articlecomment.ArticleCommentDO;
 import cn.iocoder.yudao.module.system.service.articlecomment.ArticleCommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import static cn.iocoder.yudao.framework.apilog.core.enums.OperateTypeEnum.EXPORT;
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
 @Tag(name = "管理后台 - 文章评论")
 @RestController
@@ -48,6 +47,7 @@ public class ArticleCommentController {
     @PutMapping("/update")
     @Operation(summary = "更新文章评论")
     @PreAuthorize("@ss.hasPermission('system:article-comment:update')")
+    @TenantIgnore
     public CommonResult<Boolean> updateArticleComment(@Valid @RequestBody ArticleCommentSaveReqVO updateReqVO) {
         articleCommentService.updateArticleComment(updateReqVO);
         return success(true);
@@ -57,6 +57,7 @@ public class ArticleCommentController {
     @Operation(summary = "删除文章评论")
     @Parameter(name = "id", description = "编号", required = true)
     @PreAuthorize("@ss.hasPermission('system:article-comment:delete')")
+    @TenantIgnore
     public CommonResult<Boolean> deleteArticleComment(@RequestParam("id") Integer id) {
         articleCommentService.deleteArticleComment(id);
         return success(true);
@@ -83,6 +84,7 @@ public class ArticleCommentController {
     @GetMapping("/page")
     @Operation(summary = "获得文章评论分页")
     @PreAuthorize("@ss.hasPermission('system:article-comment:query')")
+    @TenantIgnore
     public CommonResult<PageResult<ArticleCommentRespVO>> getArticleCommentPage(@Valid ArticleCommentPageReqVO pageReqVO) {
         PageResult<ArticleCommentDO> pageResult = articleCommentService.getArticleCommentPage(pageReqVO);
         return success(BeanUtils.toBean(pageResult, ArticleCommentRespVO.class));
